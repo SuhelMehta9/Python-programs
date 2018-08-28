3b16e97e210dcf47554e33c0cea3705f8eb1f79a
SuhelMehta9 <41117419+SuhelMehta9@users.noreply.github.com>
2018-08-28 23:03:55 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# M = monthly payement
+# L = Loan amount
+#i = intrest rate(for an intrest of 5%, i = 0.05)
+
+L = input('Enter the loan amount: ')
+
+i = input('Enter intrest rate: ')
+
+n = input('Enter number of years: ')
+numberOfmonths = float(n)*12
+LoanAmount = float(L)
+IntrestRate = float(i)
+M = LoanAmount*(IntrestRate*(1+IntrestRate)*numberOfmonths)/((1+IntrestRate)*numberOfmonths - 1)
+print('You must give INR {0:.2f} every month'.format(float(M)))
