ca00786a29eadd9c039da468e9dc743087595b37
SuhelMehta9 <41117419+SuhelMehta9@users.noreply.github.com>
2018-08-28 23:11:40 +0530

    Update Abberivation.py

--- Abberivation.py
+++ Abberivation.py
@@ -1,8 +1,5 @@
-
-# coding: utf-8
-
-# In[7]:
-
+# Some do not understand the abberviations like LOL etc. so to make it easy to read for them this program takes whole message and return
+# the full message.
 
 filename = 'abberivations.txt' # Storing the name of file in variable 
 usrInput = input('Enter message here\n>> ').lower() # Taking input from user
@@ -14,7 +11,7 @@ fileContent=content.split('\n') # Converting content into list
 for word in usrInput_list:
     index = usrInput_list.index(word)
     if word in fileContent: # Finding usrInput_list content in fileContent
-        if word != 'hello':
+        if word != 'hello': # hello is also in file so inorder to ignore this type of change this code is added
             index2 = fileContent.index(word)
             usrInput_list[index] = fileContent[(index2)+1] # Updating the value
 output = str(' '.join(usrInput_list)) # Converting the list into string 
