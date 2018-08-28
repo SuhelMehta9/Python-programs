9b2326411c16e288232380be85512e6c559b0de8
SuhelMehta9 <41117419+SuhelMehta9@users.noreply.github.com>
2018-08-28 23:20:58 +0530

    Update File handling.py

--- File handling.py
+++ File handling.py
@@ -1,4 +1,4 @@
-#MODULE 11
+# This program is to write to a file while the user want.
 WRITE = 'w'
 READ = 'r'
 APPEND = 'a+'
@@ -10,7 +10,7 @@ if answer == 'y':
         file = open(filename, mode = APPEND)
         file.write(("\n"))
         file.write(input('Start typing: \n'))
-        answer = input('Do you want to write file again?(y/n): ')
+        answer = input('Do you want to write file again?(y/n): ').lower()
         file.close()
 else:
     print('Exiting')
