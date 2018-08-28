f2792c61c0f18e5bbbfea94421ed105e8f176451
SuhelMehta9 <41117419+SuhelMehta9@users.noreply.github.com>
2018-08-28 23:54:22 +0530

    Create Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,12 @@
+import turtle
+for steps in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.backward(100)
+turtle.left(45)
+turtle.color('red')
+turtle.forward(141.421356)
+for steps in range(3):
+    turtle.right(90)
+    turtle.color('red')
+    turtle.forward(141.421356)
